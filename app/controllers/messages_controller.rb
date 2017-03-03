da6b20aa85ed331d1bd0343bbedee4dfccc16e86
Adam J Batson <adamjbatson@gmail.com>
2017-03-03 17:21:59 -0500

    Fix json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+  skip_before_filter :verify_authenticity_token
 
   # GET /messages
   # GET /messages.json
