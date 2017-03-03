f065f7af5d023b7a69ec44c7429b47ba0269395b
Richard Carson <rscarson@rogers.com>
2017-03-03 13:46:14 -0500

    Add sorting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
 		@current_book = ( params.has_key?(:id) ) ? 
 			Guestbook.where(id: params[:id].to_i).first : 
 			@default_book
+
+		@sort_by = params.has_key?(:sort_by) ? params[:sort_by].to_sym : :recent
+		@messages = @current_book.sorted_messages(@sort_by)
   	@guestbooks = Guestbook.all.where.not(id: @current_book.id).where.not(id: @default_book.id)
   end
 
