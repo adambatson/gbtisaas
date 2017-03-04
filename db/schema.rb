b7523f96d887f2cf7c34f40777c80ab40a3d06fc
Adam J Batson <adamjbatson@gmail.com>
2017-03-04 17:27:04 -0500

    New guestbooks default to not archived

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304014944) do
+ActiveRecord::Schema.define(version: 20170304222448) do
 
   create_table "guestbooks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
-    t.boolean  "archived"
+    t.boolean  "archived",                  default: false
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
     t.boolean  "is_default",                default: false
